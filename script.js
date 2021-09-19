cd30efb8abd6e984d33a257046bf597bb5ce8964
Mel Incognito <melxincognito@gmail.com>
2021-09-19 13:59:34 -0700

    remove book card button working

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+// UI links
+
 
 var addBook = document.getElementById("addBtn");
-var logIn = document.getElementById("logIn")
+var logIn = document.getElementById("logIn");
 var bookContainer = document.getElementById("bookCards");
 var modal = document.getElementById('modal');
 var logInModal = document.getElementById('logInModal')
 var spain = document.getElementsByClassName("close")[0];
-var submitBtn = document.getElementById('submit')
+var submitBtn = document.getElementById('submit');
 
-let myLibrary = [];
+// event listeners 
+
 
 addBook.onclick = function() {
     modal.style.display = "block";
@@ -27,8 +30,10 @@ window.onclick = function(event) {
     }
 }
 
+// Library logic 
 
 
+let myLibrary = [];
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet")
 const meli = new Book("Mel Incognito", "Mel", "33", "has read")
@@ -45,39 +50,53 @@ function Book(title, author, pages, read) {
     this.pages = pages; 
     this.read = read; 
     this.info = function() {
-        print(title + ", by " + author + ", is " + pages + " pages long, this book is " + read)
+        print(title + ", by " + author + ", is " + pages + " pages long, this book is " + read);
     }
     
 }
 
+// display logic 
 
 function addToBookLibrary(addNewBook) {
     new Book(title, author, pages, read);
     myLibrary.push(addNewBook);
 }
 
+function removeBook(book) {
+    const index = myLibrary.indexOf(book)
+    
+}
+
 
 Book.prototype.makeCard = function() {
     const bookCard = document.createElement('div');
-
+    const deleteBtn = document.createElement('button')
     const title = document.createElement('h3');
     const author = document.createElement('h3');
     const pages = document.createElement('h3');
     
     bookCard.classList.add("bookCard");
-    title.innerHTML = `${this.title}`;
-    author.textContent = `${this.author}`;
-    pages.textContent = `${this.pages} pages`;
+    deleteBtn.classList.add("dltBtn")
+    title.textContent = `Title: ${this.title}`;
+    author.textContent = `Author: ${this.author}`;
+    pages.textContent = `${this.pages} pages long`;
+    deleteBtn.textContent = 'delete book';
+
+    deleteBtn.addEventListener('click', () => {
+        bookContainer.removeChild(bookCard)
+
+    })
 
     bookCard.appendChild(title);
     bookCard.appendChild(author);
     bookCard.appendChild(pages);
-    bookContainer.appendChild(bookCard)
+    bookCard.appendChild(deleteBtn)
+    bookContainer.appendChild(bookCard);
 }
 
 myLibrary.forEach((Book) => {
-    Book.makeCard()
-})
+    Book.makeCard();
+});
 
 
 
